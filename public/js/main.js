18fbb8ceab37625169ccb607b6e7ac74e2d7a8e8
Taran Ostenson <tlostenson@gmail.com>
2016-02-11 12:21:37 -0700

    more maps

---
+++ public/js/main.js
@@ -0,0 +1,3 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoidGxvc3RlbnNvbiIsImEiOiJjaWdheWlseXMxbjJ4d25rbzk2MGppMTZxIn0.HHjHSOxre0no93YdRrVjPA';
+var map = L.mapbox.map('map', 'mapbox.streets')
+    .setView([40, -74.50], 9);
